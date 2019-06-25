3c5aa02f5e75fa0bd82fa86f1dbd913125cd2b23
Sinidrion <52216001+Sinidrion@users.noreply.github.com>
2019-06-25 23:52:57 +0300

    Add files via upload
    Должок

---
+++ HomeWorkOne.java
@@ -0,0 +1,16 @@
+public class HomeWorkOne{
+    public static void main(String[] args){
+        int itemNomber = 456;
+        float quantityOfProducts = 50.9f;
+	char groupProducts = 'B';
+	boolean mariage = false;
+	boolean receptionOfGoods = true;
+	System.out.println("Приемка товаров");
+	System.out.println("Код товара " + itemNomber);
+	System.out.println("Количество товара " + quantityOfProducts);
+	System.out.println("Группа товаров " + groupProducts);
+	System.out.println("Брак " + mariage);
+	System.out.println("Принять товар " + receptionOfGoods);
+    }
+}
+	
